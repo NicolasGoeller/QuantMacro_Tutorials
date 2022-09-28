ade316bf84fdbeb3b66ec4055b9eb8c4bdbca13c
Nicolas <n.goeller@zeppelin-university.net>
2022-09-28 22:28:03 +0200

    Broyden now working in proof-of-concept

--- PS 1/ncgm_broyden.m
+++ PS 1/ncgm_broyden.m
@@ -1,22 +1,37 @@
-function out = ncgm_broyden(guess, jacob_guess, convergence_crit, ncgm_par)
-
-T = (length(guess)-1)/2;
-%guess = [x(1:T,1); x(T+1:end,2)];
+function out = ncgm_broyden(guess, jacob_guess, convergence_crit, maxiter, ncgm_par)
+%function for executing broydens method of rootfinding
+% jacob_guess: initial value of Jacobian matrix
+% convergence_crit: criterion for accepting the convergence of the equations
+% maxiter: maximim number of iterations before breaking
 error = ncgm_seq(guess, ncgm_par);
 B = jacob_guess;
+iter = 0;
 
+%check if ssum squared of dev from 0 in equation system
 while ncgm_error(error) > convergence_crit
-    s = -inv(B)*ncgm_seq(guess, ncgm_par);  
-    guess_new = guess + s;
+    iter = iter + 1;
+    %Compute difference of new guess
+    s = -inv(B)*ncgm_seq(guess, ncgm_par); 
+    size(s)
+    size(guess)
+    
+    %compute new guess 
+    guess_new = guess + s; 
+    %Compute function value difference for two guesses
     y = ncgm_seq(guess_new, ncgm_par) - ncgm_seq(guess, ncgm_par);
-    guess_new[]
+    %y = ncgm_seq(guess_new) - ncgm_seq(guess);
+   
     guess = guess_new;
 
-    error = ncgm_seq(guess, ncgm_par);
+    error = ncgm_seq(guess, ncgm_par); %evaluate error of new guess
 
-    if abs(s'*s) > 1e-2
-        B = B + ((y-B*s)*s')/(s'*s);
+    if abs(s'*s) > 1e-2 % evaluate if norm of x difference is not 0
+        B = B + ((y' - B*s)*s')/(s'*s); %Compute updated Jacobian
     end
+    if iter > maxiter % if iteration reaches very large value terminate loop 
+        break
+    end
+
     out = guess;
 end
 
