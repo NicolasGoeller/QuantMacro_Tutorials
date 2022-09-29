6f9728570a7676a61804019bb8b43c0528918148
CorentinGuery <corentin.guery.pro@gmail.com>
2022-09-29 15:56:26 +0200

    Multiple Shooting commit

--- PS 1/NGCM_linearised_det_sequence_start.m
+++ PS 1/NGCM_linearised_det_sequence_start.m
@@ -13,7 +13,7 @@ close all
 alpha=0.4; % capital share was named theat before
 beta = 0.99; % discount factor
 %rho = 0.95;   % persistence of TFP shock this doesnt eist in the PS
-sigma = 1.00001; % CRRA coefficient (for 1 equals log, but need to replace the function, so set close to 1)
+sigma = 1.0001; % CRRA coefficient (for 1 equals log, but need to replace the function, so set close to 1)
 delta=1;
 
 % ============
@@ -21,21 +21,22 @@ delta=1;
 % ============
 
 criter_V = 1e-7; % conv criterion for value function
-T=100; % periods for transition
+T=10; % periods for transition
 
 %mean of capital non-stochastic steady state
 kbar=((1/beta-1+delta)/(alpha))^(1/(alpha-1)); %% here we inserted a *beta
+cbar= kbar^alpha-delta*kbar
 % initial level of capital in the transition
 k_0=kbar*0.75; % you may have to change this from the problem set instructions
 
 
-% ==============
+%% ==============
 % 1. analytical case delta=1, finite T and infinite T
 % ==============
 
 % a. analytical policies, finite and infinite horizon
 % use fsolve
-
+%% Result of the analytical part
 
 alphabeta = zeros(1,T); %creating alpahbeta matrix with alphabeta(i)=(alpha*beta)^i
 alphabeta(1)= 1;
@@ -52,7 +53,7 @@ if delta==1 % only makes sense for delta=1
     k_analyt(1)=k_0;
     k_analyt_finite(1)=k_0;
     for t=2:T
-        %% use the recursive expression of k calculated in Part1.3
+        % use the recursive expression of k calculated in Part1.3
         k_analyt(t)= (1-1/(sum(alphabeta(1:T-t+2))))*k_analyt(t-1)^alpha ; %sum to T-t+2 because we begin at 1 and not 0
         k_analyt_finite(t)= (1-(1/sum(alphabeta(1:T-t+2))))*k_analyt_finite(t-1)^alpha;
         % k_analyt_finite(t)=[xxxx fill this in xxxx];
@@ -63,80 +64,19 @@ if delta==1 % only makes sense for delta=1
     consum_analyt(T)=k_analyt(T);
 end
 
-
-%% This is nico's generator for system of equations
-
-
-%%
-
-% =====================
-% 4. Log-linearization
-% =====================
-
-% some ratios as function of parameters
-ybar=kbar^alpha;
-cbar=ybar-delta*kbar;
-% need for matrix form of loglin
-ckrat=cbar/kbar;
-R=1/beta;
-margprod=R-1+delta;
-
-% a. write system as A E[y_t+1]+B y_t=0
-
-
-
-% order c,k,z
-A=[-sigma, beta *(alpha-1)*alpha*kbar^(alpha-1) ; 0 , 1 ];
-
-B= [-sigma , 0 ; -ckrat,-alpha*kbar^(alpha-1)];
-
-D = A'*B;
-
-% note that these are right-hand eigenvectors
-[ ev lambda]=eig(D); %%%give the egein vectors and eigen values of D
-aaa=inv(ev); %%% give the invert of the eigen vector matrix
-
-% find eigenvalues equal or larger than one, and check that they equal the
-% number of jump variables - in that case, set BKcond to 1
-BKcond = abs(diag(lambda))
-
-if BKcond~=1
-    disp('BK conditions not satisfied')
-else
-    indic =find(abs(diag(lamba))>1);
-    indic1= find(abs(diag(lambda))<=1);
-    polfunc_temp=aaa(indic,:);
-    polfunc= -polfunc_temp(1,2)/polfunc_temp(1);% you need to find the policy for consumption here
-end
-
-% policy functions are in log deviations. so need to pre-multiply by cbar
-% and add cbar to get levels
-
-clev=cbar+cbar*polfunc(1)*(kgrid-kbar)/kbar;
-kprimelev=kgrid'.^alpha+(1-delta)*kgrid'-clev(:,i);
-
-% calculate the deterministic transition  using the linearised policy
-% functions, and law of motion
-k_lin(1)=k_0;
-for t=2:T
-    c_lin(t-1)=[xxxx fill this in xxxx] 
-    k_lin(t)=[xxxx fill this in xxxx] 
-end
-
-
-% ==============
+%% ==============
 % 4. Solve deterministic sequence
 % ==============
 
 % as usual we need an initial guess for the sequences of k and c - here we
 % just use steady state
 x0=[kbar*ones(T,1);cbar*ones(T,1)];
-
-zpath=ones(T,1);
-zpath(1)=1.01;
-for i=2:T-1
-zpath(i)=exp(log(zpath(i-1)));
-end
+%%
+% zpath=ones(T,1);
+% zpath(1)=1.01;
+% for i=2:T-1
+%     zpath(i)=exp(log(zpath(i-1)));
+% end
 
 % now we need a function that returns errors of the equation system,
 % constraining k(0)=k_0, and c(T)=cbar
@@ -145,29 +85,53 @@ end
 
 %Initial guess for jacobian - use finite difference at steady state
 %sequences of k and c
-
+%rbc_obj(x,alpha,beta,sigma,delta,kbar,cbar)
+%%
 clear dx J
 for i=1:2*T
     dx = zeros(2*T,1);
     dx(i)=x0(i)*0.001;
-    J(:,i)=[xxxx fill this in xxxx] ;
+    diffF = rbc_obj_start(x0+dx,alpha,beta,sigma,delta,kbar,cbar)-rbc_obj_start(x0,alpha,beta,sigma,delta,kbar,cbar);
+    diffX= dx(i);
+    J(:,i)=[diffF/diffX] ;
 end
-
-crit=1e-10;
+clear x
+%%
+crit=1e-4;
 x=x0;
-f=rbc_obj(x,alpha,beta,sigma,delta,k_0,cbar,zpath);
-
+f0=rbc_obj_start(x,alpha,beta,sigma,delta,k_0,cbar);
+f=rbc_obj_start(x,alpha,beta,sigma,delta,k_0,cbar);
+%%
 while max(abs(f))>crit
 
-dx = [xxxx fill this in xxxx] 
-x=x+dx;
-f = rbc_obj(x,alpha,beta,gamma,delta,k_0,cbar,ones(T,1));
-J = [xxxx fill this in xxxx] ;
+%dx = [xxxx fill this in xxxx] 
+%     dx = zeros(2*T,1);
+%     for i=1:2*T
+%         for j=1:2*T
+%             dx(i)= dx(i)-J(i,j)^(-1)*f(i);
+%         end
+%     end
+    f0 = rbc_obj_start(x,alpha,beta,sigma,delta,k_0,cbar);
+    dx = -J\f; 
+    %dx = dx*(x'*x)/(10*(dx'*dx));
+    %while x+dx<1e-6
+      %  dx=dx/2;
+    %end
+    xn1=max(ones(2*T,1)*1e-8,x+dx);
+    dx=xn1-x;
+    f = rbc_obj_start(xn1,alpha,beta,sigma,delta,k_0,cbar);
+    %J = [] ;
+    J = J + ((f-f0)-J*dx)*dx'/(dx'*dx);
+    %B = B + ((y' - B*s)*s')/(s'*s);
+    x=xn1;
+
 
 end
 
-k_trans_br=x(1:T,1);
-c_trans_br=x(T+1:2*T,1);
+
+%%
+%k_trans_br=x(1:T,1);
+%c_trans_br=x(T+1:2*T,1);
 
 % or we just let matlab solve it
 
@@ -217,20 +181,20 @@ T = 10;
 params.k0 = 0.75*kbar;
 
 % Compile inputs
-%kt = [ones(T,1)*0.75*kbar; 0];
-%ct = [ones(T,1)*0.8*cbar; 0];
-kt = ones(T,1)*0.75*kbar;
-ct = ones(T,1)*0.8*cbar;
+kt = [ones(T,1)*0.75*kbar; 0];
+ct = [ones(T,1)*0.8*cbar; 0];
+%kt = ones(T,1)*0.75*kbar;
+%ct = ones(T,1)*0.8*cbar;
 x = [kt; ct];
-jacob = eye(2*T+1); %Jacobian guess as identity matrix
+jacob = eye(2*T); %Jacobian guess as identity matrix
 %jacob
 %a = ncgm_seq(x, params)
 %%
 
 a= ncgm_seq(x, params)
 %fsolve(@ncgm_seq, x)
-
-%ncgm_broyden(x, jacob, 1e-4, 50, params)
+%%
+ncgm_broyden(x, jacob, 1e-4, 50, params)
 
 %%
 
