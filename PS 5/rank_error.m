646044edb8b21ec7f5156b6551102b997af89226
Nicolas <n.goeller@zeppelin-university.net>
2022-10-26 23:44:13 +0200

    Completed RANK error under RE; added broyden RANK

--- PS 5/rank_error.m
+++ PS 5/rank_error.m
@@ -14,12 +14,12 @@ error = zeros(length(x),1);
 
 kappa = (params.sigma + params.vartheta)*(1-params.theta)*(1-params.beta*params.theta)/params.beta;
 
-% calculate errors in budget constraint - capital k
+% Here we assume bc of RE that E(x_{t+1}) = x_{+1}
 for t=1:T
     % DIS equation erros
-    error(t,1)=  x(t,1) + params.sigma^(-1)*(x(2*T+t,1) - x()) - x();
+    error(t,1)=  x(t,1) + params.sigma^(-1)*(x(2*T+t,1) - x(T+t+1,1)) - x(t+1,1);
     % NKPC equation error
-    error(T+t,1) = x(T+t,1) - params.beta*x() - kappa*(x(t,1) - params.varphi*z(t,1));
+    error(T+t,1) = x(T+t,1) - params.beta*x(T+t+1,1) - kappa*(x(t,1) - params.varphi*z(t,1));
     % taylor rule error
     error(2*T+t,1) = x(2*T+t,1) - params.phipi*x(T+t,1) + z(T+t,1);
 end
