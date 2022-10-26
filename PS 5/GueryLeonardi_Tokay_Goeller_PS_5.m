58ad6b6d577160e9819c207bafcbf0f1ab21ad75
CorentinGuery <corentin.guery.pro@gmail.com>
2022-10-26 23:43:30 +0200

    TANK Broyden step 1

--- PS 5/GueryLeonardi_Tokay_Goeller_PS_5.m
+++ PS 5/GueryLeonardi_Tokay_Goeller_PS_5.m
@@ -22,14 +22,14 @@ params.rhoa = 0.95 ; %AR1 parameter for labor productivity
 params.rhonu = 0.5 ; %AR1 parameter for monetary policy shocks
 params.lambda = 0.3 ; %share of hands-to-mouth households
 params.taud = 0.1; %firm profit redistribution share
-params.vartheta = %Frisch elasticity of labor supply
+params.vartheta = 0.666667; %Frisch elasticity of labor supply
 
 % =============
 % Shock parameters
 % =============
 
-
-
+params.epsilon_nu = 0.005; %monetary policy shock
+params.epsilon_a = 0.005; %productivity shock
 
 % ============
 % options and convergence criteria
@@ -43,54 +43,6 @@ params.T=100; % periods for transition
 % ===========
 
 
-
-% ==============
-% 2. Solving deterministic equation systems
-% ==============
-
-% a&b. analytical policies, finite and infinite horizon; delta = 1
-
-%% Analytical part and sequence
-alphabeta = zeros(1,T); %creating alpahbeta matrix with alphabeta(i)=(alpha*beta)^i
-alphabeta(1)= 1;
-for t=2:T
-    alphabeta(t)=alphabeta(t-1)*alpha*beta;
-end
-
-if delta==1 % only makes sense for delta=1
-    k_analyt=zeros(1,T);
-    consum_analyt=zeros(1,T); %vectors with consumption at t
-    consum_analyt_finite=zeros(1,T);
-    
-    k_analyt(1)=k_0;
-    k_analyt_finite(1)=k_0;
-    for t=2:T
-        % use the recursive expression of k calculated in Part1.3
-        k_analyt(t)= (1-1/(sum(alphabeta(1:T-t+2))))*k_analyt(t-1)^alpha ; %sum to T-t+2 because we begin at 1 and not 0
-        k_analyt_finite(t)= (1-(1/sum(alphabeta(1:T-t+2))))*k_analyt_finite(t-1)^alpha;
-        % k_analyt_finite(t)=[xxxx fill this in xxxx];
-        consum_analyt(t-1)=k_analyt(t-1)^alpha - k_analyt(t);
-        consum_analyt_finite(t-1)=k_analyt_finite(t-1)^alpha - k_analyt_finite(t);
-    end
-    consum_analyt_finite(T)=k_analyt_finite(T);
-    consum_analyt(T)=k_analyt(T);
-end
-
-%%
-
-% c. numerical solution algorithms to above problems
-
-%set guesses
-kbar=((1/params.beta-1+params.delta)/(params.alpha*params.beta))^(1/(params.alpha-1));
-cbar = kbar^params.alpha-params.delta*kbar;
-
-params.k0 = 0.75*kbar;
-params.cbar = cbar;
-
-% Compile inputs
-%kt = [ones(T,1)*0.75*kbar; 0];
-%ct = [ones(T,1)*0.8*cbar; 0];
-
 %% Blueprint for solution
 
 % Define equilibrium error function
@@ -98,23 +50,23 @@ params.cbar = cbar;
 % - RANK model
 
 % Set initial guess vectors to eq-ss values
+x_tank_init=zeros(4*T,1);
+x_rank_init=zeros(3*T,1);
+
 
 % Set perturbance vectors to assigned t=0 shock
-epsi_ = [0.005; zeros(T,1)];
+epsi_nu = [0.005; zeros(T,1)];
 epsi_a = zeros(T+1,1);
 
 % start Broyden Function (including initial jacobian)
 
 
 
-%% Broydens Method
+%% Broydens Method for TANK model
 
 % Broydens method Finite time T=10
 T1 = 10;
-kt1 = ones(T1,1)*0.75*kbar;
-ct1 = ones(T1,1)*0.8*cbar;
-x1 = [kt1; ct1];
-jacob1 = ncgm_jacob(x1, params);
+jacob_tank = ncgm_jacob(x1, params);
 trans1 = ncgm_broyden(x1, jacob1, 1e-6, T1, params);
 
 
